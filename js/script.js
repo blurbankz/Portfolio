4ccc3625718964266389831d57342ba38256bbd9
Natthaphong Milathako <38761679+blurbankz@users.noreply.github.com>
2023-02-17 12:49:26 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
-$(document).ready(function() {
-    $(".btn-hamburger").on("click",  function(){
-        $("nav ul").toggleClass("nav-active");
-    })
+
+// NavBar
+$(document).ready(function () {
+  $(".btn-hamburger").on("click", function () {
+    $("nav ul").toggleClass("nav-active");
+  })
 });
 
-function myFunction(x) {
-    x.classList.toggle("change");
-  }
-  
 
-  $(document).ready(function () {
-  
-    $(".nav__link").click(function (){
-      $(this).addClass("active").siblings().removeClass("active");
-    });
-  });
-  
+// ScrollReveal Animations
+ScrollReveal({
+  reset: false,
+  distance: "60px",
+  duration: 2500,
+  delay: 400
+});
+
+ScrollReveal().reveal('.home .content', { origin: "left", delay: 500 });
+ScrollReveal().reveal('.home .image', { origin: "right", delay: 500 });
+ScrollReveal().reveal('.about .row, .skills .row, .portfolio .row, .contact .row', { origin: "bottom", delay: 500 })
